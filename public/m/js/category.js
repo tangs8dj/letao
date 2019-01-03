65377f09bd47a61d2a3c7bd0f798d33cb12f7a10
tangs8dj <tangs8dj@hotmail.com>
2019-01-03 13:33:33 +0800

    1.3中午暂存,新增分类模板

--- public/m/js/category.js
+++ public/m/js/category.js
@@ -4,4 +4,22 @@ $(function () {
         indicators: false, //是否显示滚动条
         deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     });   
+    $.ajax({
+        url:"/category/queryTopCategory",
+        success: function (data){
+            // console.log(data);
+            var html = template("tpl-left",data);
+            console.log(html);
+            $('.content-left ul').html(html)
+        }
+    })
+    $.ajax({
+        url:"/category/querySecondCategory",
+        success: function (data){
+            console.log(data);
+            var html = template("tpl-right",data);
+            console.log(html);
+            $('.mui-row').html(html)
+        }
+    })
 })
